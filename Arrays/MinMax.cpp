a6e2739423ef99b63d5c3e343e69e1f9a65216c2
Varun Kumar Reddy Pothula <117117037+Bunny2906@users.noreply.github.com>
2022-12-08 21:43:54 +0530

    Add files via upload

---
+++ Arrays/MinMax.cpp
@@ -0,0 +1,41 @@
+//Finding minimum and maximum elements in Array in 'Single Scan'
+#include<iostream>
+using namespace std;
+struct Array
+{
+int size;
+int *A;
+};
+int minMax(struct Array arr)
+{
+int i,j,min,max;
+min=max=arr.A[0];
+for(i=1;i<arr.size;i++)
+{
+    if(arr.A[i]<min)
+    {
+        min=arr.A[i];
+    }
+    else if(arr.A[i]>max)
+    {
+        max=arr.A[i];
+    }
+}
+cout<<"maximum element is :"<<max<<endl;
+cout<<"minimum element is :"<<min<<endl;
+return 0;
+}
+int main()
+{
+    struct Array arr;
+    cout<<"Enter the size of array :";
+    cin>>arr.size;
+    arr.A=new int[arr.size];
+    cout<<"Enter array elements :\n";
+    for(int i=0;i<arr.size;i++)
+    {
+        cin>>arr.A[i];
+    }
+    minMax(arr);
+    return 0;
+}
