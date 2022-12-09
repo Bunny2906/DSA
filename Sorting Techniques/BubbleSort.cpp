5f0545280efca4c0e156e4120f4587f1f3fa48ce
Varun Kumar Reddy Pothula <117117037+Bunny2906@users.noreply.github.com>
2022-12-09 14:05:02 +0530

    Update BubbleSort.cpp

--- Sorting Techniques/BubbleSort.cpp
+++ Sorting Techniques/BubbleSort.cpp
@@ -1,3 +1,17 @@
+//Bubble Sort using Array
+/*-->Each element of array is compared with its adjacent element.
+  -->The algorith processes the list in passes. A list with n elements requires (n-1) passes for sorting.
+  -->In first pass,first element is compared with second element and if first element is  greater than second element,then they are swapped.
+     Similarly,comparison is done for next two elements and then for entire list.
+  -->After first pass,The largest element in the list is at last position in the list.Since we already got our largest
+     element at last position,we no need to compare it again.
+     In second pass we follow the same process and comparison is done only for (n-2) elements.After second pass,
+     second largest element is at second position from the last in the list.
+   -->Same process is followed until (n-1) passes and then list gets sorted.  
+*/
+/*-->Bubble sort is adaptive because it takes minimum time when list is already sorted or partially sorted. We made it adaptive by introducing 'flag' bit.
+  -->Bubble sort is stable because it will sort the list even if it contains duplicates.*/
+
 #include<iostream>
 using namespace std;
 struct Array
@@ -26,19 +40,31 @@ void bubbleSort(struct Array arr)
 int main()
 {
     struct Array arr;
-    cout<<"enter the size of array :";
+    cout<<"enter the size of list :";
     cin>>arr.n; 
     arr.A=new int[arr.n];
-    cout<<"enter array elements :\n";
+    cout<<"enter list elements :\n";
     for(int i=0;i<arr.n;i++)
     {
         cin>>arr.A[i];
     }
     bubbleSort(arr);
-    cout<<"Sorted Array is:";
+    cout<<"Sorted List is:";
     for(int i=0;i<arr.n;i++)
     {
         cout<<arr.A[i]<<" ";
     }
     return 0;
 }
+/*
+Sample Output:
+enter the size of list :5
+enter list elements :
+30
+40
+10
+50
+20
+Sorted List is:10 20 30 40 50
+*/
+
